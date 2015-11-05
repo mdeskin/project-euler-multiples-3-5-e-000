ff0bc5f0611200a37b34478d406a69966c5bb254
mdeskin <michaelhaydendeskin@gmail.com>
2015-11-05 21:42:04 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,23 @@
-# Enter your procedural solution here!
+
+def collect_multiples(limit)
+  upto = []
+  counter = 1
+   while counter < limit
+      if counter % 3 == 0 || counter % 5 == 0
+        upto << counter
+      end
+     counter += 1
+   end
+  upto
+end
+
+def sum_multiples(limit)
+  total = 0
+  element = 0
+  myArray = collect_multiples(limit)
+  until myArray[element] == nil
+    total = myArray[element] + total
+    element += 1
+   end
+total
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,34 @@
-# Enter your object-oriented solution here!
+class Multiples
+
+   def initialize(limit)
+     @limit = limit
+   end
+  def limit=(limit)
+    @limit = limit
+  end
+  def limit
+    @limit
+  end
+
+  def collect_multiples
+        myArray = []
+        counter = 1
+         while counter < @limit
+            if counter % 3 == 0 || counter % 5 == 0
+              myArray << counter
+            end
+           counter += 1
+         end
+         myArray
+    end
+
+    def sum_multiples
+      total = 0
+      element = 0
+      until collect_multiples[element] == nil
+         total = collect_multiples[element] + total
+         element += 1
+      end
+    total
+    end
+  end
